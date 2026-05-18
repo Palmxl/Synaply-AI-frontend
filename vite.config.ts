e81b7a31a9be51c9e957052553438df8a912fc90
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-05-17 21:01:57 -0500

    config: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+
 import path from "path"
 
 export default defineConfig({
+  base: "/synaply-ai-frontend/",
+
   plugins: [
     react(),
     tailwindcss(),
   ],
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(
+        __dirname,
+        "./src"
+      ),
     },
   },
 })
