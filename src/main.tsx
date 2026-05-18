a7acda902e24911ad7957f7724ab99123357a789
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-05-17 21:10:08 -0500

    config: github pages deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,42 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { Toaster } from "@/components/ui/sonner"
+
+import {
+  BrowserRouter,
+} from "react-router-dom"
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query"
+
+import {
+  Toaster,
+} from "@/components/ui/sonner"
 
 import App from "./App"
 
 import "./index.css"
 
-import { AuthProvider } from "./context/AuthContext"
+import {
+  AuthProvider,
+} from "./context/AuthContext"
 
 const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider
+      client={queryClient}
+    >
       <AuthProvider>
-        <BrowserRouter>
+        <BrowserRouter
+          basename="/Synaply-AI-frontend"
+        >
           <App />
+
           <Toaster />
         </BrowserRouter>
       </AuthProvider>
