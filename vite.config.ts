c00cdf8d3f405ef17ba82a12364467d264b60910
Palmxl <jmartin_sanchez@javeriana.edu.co>
2026-05-17 21:05:01 -0500

    config: github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
 export default defineConfig({
-  base: "/synaply-ai-frontend/",
+  base: "/Synaply-AI-frontend/",
 
   plugins: [
     react(),
